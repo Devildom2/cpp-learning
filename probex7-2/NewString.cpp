b588a30e7e21bc6ad1b4e702da038f255370e252
Devildom2 <yagasaki.biz.t@gmail.com>
2022-09-27 11:06:22 +0900

    一週間でC++終了

--- probex7-2/NewString.cpp
+++ probex7-2/NewString.cpp
@@ -1,5 +1,6 @@
+#include <cstring>
 #include "NewString.h"
- 
+
 //  コンストラクタ
 NewString::NewString()
 {
@@ -11,16 +12,26 @@ NewString::NewString(string value)
     m_value = value;
 }
 //  値を代入するコンストラクタ②（他のクラスから)
-NewString::NewString(NewString& value)
+NewString::NewString(NewString &value)
 {
     m_value = value.getValue();
 }
 //  値を代入
-NewString& NewString::operator= (NewString& n)
+NewString &NewString::operator=(NewString &n)
 {
     m_value = n.getValue();
     return *this;
 }
+//  値を比較
+bool NewString::operator==(NewString &n)
+{
+    bool res = false;
+    if( !strcasecmp(m_value.c_str(), n.getValue().c_str() )){
+        res = true;
+    }
+    return res;
+}
+
 //  stringで値を取得
 string NewString::getValue()
 {
--- probex7-2/main.cpp
+++ probex7-2/main.cpp
@@ -13,5 +13,11 @@ int main(){
     cout << s3.getValue() << endl;
     NewString s4("HELLOWORLD");
     cout << s4.getValue() << endl;
+    if(s1 == s2){
+        cout << "s1==s2" << endl;
+    }
+    if(s1 == s4){
+        cout << "s1==s4" << endl;
+    }
     return 0;
 }
